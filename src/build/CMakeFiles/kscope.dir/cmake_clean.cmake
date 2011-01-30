b16db406418f205e5cd4cd4db93c48482210afc7
follinge <follinge@gmail.com>
2011-01-30 06:39:47 +0000

    Added some files. Now we have one more dependency that kind works.

---
+++ src/build/CMakeFiles/kscope.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+FILE(REMOVE_RECURSE
+  "kscope_automoc.cpp"
+  "kscope.shell"
+  "CMakeFiles/kscope.dir/kscope_automoc.o"
+  "CMakeFiles/kscope.dir/kscopeconfig.o"
+  "CMakeFiles/kscope.dir/main.o"
+  "CMakeFiles/kscope.dir/kscope.o"
+  "kscope.pdb"
+  "kscope"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/kscope.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
