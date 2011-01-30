62ebac306292c307144dab880a68400857ec11b6
follinge <follinge@gmail.com>
2011-01-30 02:21:26 +0000

    Added a bunch of new files.

--- src/kscope.cpp
+++ src/kscope.cpp
@@ -1,42 +1,59 @@
-/***************************************************************************
- *
- * Copyright (C) 2005 Elad Lahav (elad_lahav@users.sourceforge.net)
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- ***************************************************************************/
-
 #include "kscope.h"
+#include <KApplication>
+#include <KAction>
+#include <KLocale>
+#include <KActionCollection>
+#include <KStandardAction>
+#include <KFileDialog>
+#include <KMessageBox>
+#include <KIO/NetAccess>
+#include <KSaveFile>
+#include <QTextStream>
+#include <KXMLGUIFactory>
+#include <KTextEditor/Document>
+#include <KTextEditor/View>
+#include <KTextEditor/Editor>
+#include <KTextEditor/EditorChooser>
 
-/**
- * Class constructor.
- * @param	pParent	The parent widget
- * @param	szName	The widget's name
- */
-//KScope::KScope(QWidget* pParent, const char* szName) 
-KScope::KScope() 
+Kscope::Kscope(QWidget *)
 {
+KTextEditor::Editor *editor = KTextEditor::EditorChooser::editor();
+
+if (!editor) {
+	KMessageBox::error(this, i18n("A KDE text-editor component could not be found;\n" "please check your KDE installation."));
+
+	kapp->exit(1);
 }
 
-KScope::~KScope()
-{
+m_doc = editor->createDocument(0);
+m_view = qobject_cast<KTextEditor::View*>(m_doc->createView(this));
+
+setCentralWidget(m_view);
+setupActions();
+
+setXMLFile("editorui.rc");
+createShellGUI(true);
+
+guiFactory()->addClient(m_view);
+
+show ();
 }
+
+void Kscope::setupActions()
+{
+KStandardAction::quit(kapp, SLOT(quit()), actionCollection());
+KStandardAction::open(this, SLOT(openFile()), actionCollection());
+KStandardAction::clear(this, SLOT(clear()), actionCollection());
+}
+
+void Kscope::clear()
+{
+m_doc->clear();
+}
+
+void Kscope::openFile()
+{
+m_view->document()->openUrl(KFileDialog::getOpenFileName());
+}
+
+// Sat Jan 29 18:31:35 UTC 2011
