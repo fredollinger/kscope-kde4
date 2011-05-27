9a367a6844974f81a49d23ba88264e9c6b1c53e7
Fred Ollinger <follinge@gmail.com>
2011-05-27 14:58:38 -0700

    These files should actually be removed.

--- src/build/moc_kscope4.cpp
+++ src/build/moc_kscope4.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'kscope4.h'
 **
-** Created: Wed May 25 14:40:07 2011
+** Created: Fri May 27 10:05:32 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
 **
 ** WARNING! All changes made in this file will be lost!
