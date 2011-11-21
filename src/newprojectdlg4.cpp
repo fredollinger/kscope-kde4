5c291765a01c5873b035d2547063997570702a9d
Fred Ollinger <follinge@gmail.com>
2011-11-20 17:17:33 -0800

    newprojectdlg is almost done.

--- src/newprojectdlg4.cpp
+++ src/newprojectdlg4.cpp
@@ -10,29 +10,32 @@
 #include <kmessagebox.h>
 #include <klocale.h>
 #include "newprojectdlg4.h"
-#include "autocompletionlayout4.h"
 
+/**
+ * Class constructor.
+ * @param	bNewProj	true to create a new project dialog, false to display
+ *						the properties of an existing project
+ * @param	pParent		The parent widget
+ * @param	szName		The widget's name
+ */
 NewProjectDlg::NewProjectDlg(bool bNewProj, QWidget* pParent, 
 	const char* szName) :
-	Ui::NewProjectLayout(),
+ 	Ui::NewProjectLayout(),
 	m_bNewProj(bNewProj)
 {
-	qDebug() << "BEGIN NewProjectDlg() \n";
- 	setupUi(this);
+ 	setupUi(this); // boilerplate for all qdialogs
 
 	ProjectBase::Options opt;
 
 	// Create the auto-completion sub-dialogue
 	m_pAutoCompDlg = new AutoCompletionDlg(this);
 	
-	qDebug() << "NewProjectDlg(): Restrict files to existing dirs... \n";
 	// Restrict the path requester to existing directories.
 	m_pPathRequester->setMode(KFile::Directory | KFile::ExistingOnly | 
 		KFile::LocalOnly);
 	m_pSrcRootRequester->setMode(KFile::Directory | KFile::ExistingOnly | 
 			KFile::LocalOnly);
 	
-	qDebug() << "NewProjectDlg(): Connect signals and slots \n";
 	// Set up the Create/Cancel buttons	
 	connect(m_pCreateButton, SIGNAL(clicked()), this, SLOT(accept()));
 	connect(m_pCancelButton, SIGNAL(clicked()), this, SLOT(reject()));
@@ -43,15 +46,13 @@ NewProjectDlg::NewProjectDlg(bool bNewProj, QWidget* pParent,
 	// Perform actions specific to the type of dialog (new project or
 	// project properties)
 	if (bNewProj) {
-		qDebug() << "NewProjectDlg(): bNewProj \n";
 		// Set default project properties
 		ProjectBase::getDefOptions(opt);
 		setProperties("", "", opt);
 	}
 	else {
-		qDebug() << "NewProjectDlg(): NOT bNewProj \n";
 		// Give appropriate titles to the dialog and the accept button
-		setWindowTitle(i18n("Project Properties"));
+ 		setWindowTitle(i18n("Project Properties"));
 		m_pCreateButton->setText(i18n("OK"));
 		
 		// Disable the non-relevant widgets
@@ -59,87 +60,13 @@ NewProjectDlg::NewProjectDlg(bool bNewProj, QWidget* pParent,
 		m_pPathRequester->setEnabled(false);
 	}
 
-	qDebug() << "END: NewProjectDlg()\n";
 }
 
+/**
+ * Class destructor.
+ */
 NewProjectDlg::~NewProjectDlg()
 {
-}
-
-
-/**
- * Fills a structure with all user-configured project options.
- * @param	opt	The structure to fill
- */
-void NewProjectDlg::getOptions(ProjectBase::Options& opt)
-{
-	/*
-	opt.sSrcRootPath = m_pSrcRootRequester->url();
-	opt.slFileTypes = m_slTypes;
-	opt.bKernel = m_pKernelCheck->isChecked();
-	opt.bInvIndex = m_pInvCheck->isChecked();
-	opt.bNoCompress = m_pNoCompCheck->isChecked();
-	opt.bSlowPathDef = m_pSlowPathCheck->isChecked();
-		
-	if (m_pAutoRebuildCheck->isChecked())
-		opt.nAutoRebuildTime = m_pAutoRebuildSpin->value();
-	else
-		opt.nAutoRebuildTime = -1;
-		
-	if (m_pTabWidthCheck->isChecked())
-		opt.nTabWidth = m_pTabWidthSpin->value();
-	else
-		opt.nTabWidth = 0;
-		
-	opt.bACEnabled = m_pACCheck->isChecked();
-	opt.nACMinChars = m_pAutoCompDlg->m_nMinChars;
-	opt.nACDelay = m_pAutoCompDlg->m_nDelay;
-	opt.nACMaxEntries = m_pAutoCompDlg->m_nMaxEntries;
-	
-	opt.sCtagsCmd = m_pCtagsCmdEdit->text();
-	*/
-}
-
-/**
- * Retrieves the text entered by the user in the dialog's "Project Name" edit
- * box.
- * @return	The name of the new project
- */
-QString NewProjectDlg::getName()
-{
-	qDebug() << "NewProjectDlg::getName\n";
-	return m_pNameEdit->text();
-}
-
-/**
- * Retrieves the text entered by the user in the dialog's "Project Path" edit
- * box.
- * Note that the chosen path will be the parent of the new project's
- * directory, created under it using the project's name.
- * @return	The full path of the parent directory for the new project
- */
-QString NewProjectDlg::getPath()
-{
-	qDebug() << "NewProjectDlg::getPath\n";
-	if (m_pHiddenDirCheck->isChecked())
-		return QString(m_pSrcRootRequester->text()) + "/.cscope";
-	
-	return m_pPathRequester->text();
-}
-
-void NewProjectDlg::slotAddType()
-{
-	qDebug() << "NewProjectDlg::slotAddType stub\n";
-}
-
-void NewProjectDlg::slotAvailTypesChanged(const QString &qstr)
-{
-	qDebug() << "NewProjectDlg::slotAvailTypesChanged stub\n";
-}
-
-void NewProjectDlg::slotRemoveType()
-{
-	qDebug() << "NewProjectDlg::slotRemoveType stub\n";
 }
 
 /**
@@ -152,13 +79,12 @@ void NewProjectDlg::slotRemoveType()
 void NewProjectDlg::setProperties(const QString& sName, const QString& sPath,
 	const ProjectBase::Options& opt)
 {
-	qDebug() << "NewProjectDlg::setProperties(): \n";
 	QStringList::ConstIterator itr;
 	
 	// Set values for current project
 	m_pNameEdit->setText(sName);
-	m_pPathRequester->setText(sPath);
-	m_pSrcRootRequester->setText(opt.sSrcRootPath);
+ 	m_pPathRequester->setText(sPath);
+ 	m_pSrcRootRequester->setText(opt.sSrcRootPath);
 	m_pKernelCheck->setChecked(opt.bKernel);
 	m_pInvCheck->setChecked(opt.bInvIndex);
 	m_pNoCompCheck->setChecked(opt.bNoCompress);
@@ -191,13 +117,67 @@ void NewProjectDlg::setProperties(const QString& sName, const QString& sPath,
 }
 
 /**
+ * Retrieves the text entered by the user in the dialog's "Project Name" edit
+ * box.
+ * @return	The name of the new project
+ */
+QString NewProjectDlg::getName()
+{
+	return m_pNameEdit->text();
+}
+
+/**
+ * Retrieves the text entered by the user in the dialog's "Project Path" edit
+ * box.
+ * Note that the chosen path will be the parent of the new project's
+ * directory, created under it using the project's name.
+ * @return	The full path of the parent directory for the new project
+ */
+QString NewProjectDlg::getPath()
+{
+	if (m_pHiddenDirCheck->isChecked())
+		return QString(m_pSrcRootRequester->text()) + "/.cscope";
+	
+	return m_pPathRequester->text();
+}
+
+/**
+ * Fills a structure with all user-configured project options.
+ * @param	opt	The structure to fill
+ */
+void NewProjectDlg::getOptions(ProjectBase::Options& opt)
+{
+	opt.sSrcRootPath = m_pSrcRootRequester->text();
+	opt.slFileTypes = m_slTypes;
+	opt.bKernel = m_pKernelCheck->isChecked();
+	opt.bInvIndex = m_pInvCheck->isChecked();
+	opt.bNoCompress = m_pNoCompCheck->isChecked();
+	opt.bSlowPathDef = m_pSlowPathCheck->isChecked();
+		
+	if (m_pAutoRebuildCheck->isChecked())
+		opt.nAutoRebuildTime = m_pAutoRebuildSpin->value();
+	else
+		opt.nAutoRebuildTime = -1;
+		
+	if (m_pTabWidthCheck->isChecked())
+		opt.nTabWidth = m_pTabWidthSpin->value();
+	else
+		opt.nTabWidth = 0;
+		
+	opt.bACEnabled = m_pACCheck->isChecked();
+	opt.nACMinChars = m_pAutoCompDlg->m_nMinChars;
+	opt.nACDelay = m_pAutoCompDlg->m_nDelay;
+	opt.nACMaxEntries = m_pAutoCompDlg->m_nMaxEntries;
+	
+	opt.sCtagsCmd = m_pCtagsCmdEdit->text();
+}
+
+/**
  * Ends the dialog after the user has clicked the "OK" button.
  */
 void NewProjectDlg::accept()
 {
 	int i, nCount;
-
-	qDebug() << "NewProjectDlg::accept() BEGIN\n";
 	
 	// Validate the name of a new project
 	if (m_bNewProj) {
@@ -225,15 +205,87 @@ void NewProjectDlg::accept()
 	QDialog::accept();
 }
 
-// BEGIN AutoCompletionDlg
+/**
+ * Adds the the file type string in the edit-box to the list of project types.
+ * This slot is called when the "Add.." button is clicked.
+ */
+void NewProjectDlg::slotAddType()
+{
+	QString sType;
+		
+	// Try the custom type edit-box first.
+	sType = m_pTypesEdit->text();
+	sType.trimmed();
+	if (sType.isEmpty())
+		return;
+
+	// Validate the type string
+	QRegExp reg("[ \\t\\n\\|\\\\\\/]");
+	if (sType.contains(reg)) {
+		KMessageBox::error(0, i18n("This is not a valid file type!"));
+		return;
+	}
+
+	// Do not add an existing type.
+	if (m_pTypesList->findItem(sType, Q3ListBox::CaseSensitive | Q3ListBox::ExactMatch) != 
+		NULL) {
+		return;
+	}
+
+	// Add the file type to the list
+	m_pTypesList->insertItem(sType);
+	m_pTypesEdit->clear();
+}
+
+/**
+ * Removes the selected item from the list of file types.
+ * This slot is called when the "Remove" button is clicked.
+ */
+void NewProjectDlg::slotRemoveType()
+{
+	int nItem;
+	QString sType;
+	
+	// Verify an item is selected
+	nItem = m_pTypesList->currentItem();
+	if (nItem == -1)
+		return;
+
+	// Remove the selected item
+	sType = m_pTypesList->currentText();
+	m_pTypesList->removeItem(nItem);
+	
+	// Add to the list of available types.
+	if (m_pAvailTypesList->findItem(sType, Q3ListBox::CaseSensitive | Q3ListBox::ExactMatch) 
+		== NULL) {
+		m_pAvailTypesList->insertItem(sType);
+	}
+	
+}
+
+/**
+ * Changes the text in the types edit-box to reflect the current selection in
+ * the list of available types.
+ * This slot is called whenever a new item is highlighted in the list of
+ * available types.
+ * @param	sType	The newly selected type
+ */
+void NewProjectDlg::slotAvailTypesChanged(const QString& sType)
+{
+	m_pTypesEdit->setText(sType);
+}
+
 /**
  * Class constructor.
  * @param	pParent		The parent widget
  * @param	szName		The widget's name
  */
 AutoCompletionDlg::AutoCompletionDlg(QWidget* pParent,
-	const char* szName ) 
+	const char* szName ) :
+	Ui::AutoCompletionLayout()
 {
+	connect(m_pOKButton, SIGNAL(clicked()), this, SLOT(accept()));
+	connect(m_pCancelButton, SIGNAL(clicked()), this, SLOT(reject()));
 }
 
 /**
@@ -241,22 +293,6 @@ AutoCompletionDlg::AutoCompletionDlg(QWidget* pParent,
  */
 AutoCompletionDlg::~AutoCompletionDlg()
 {
-}
-
-/**
- * Stores the values set by the user in the dialogue widgets, and terminates
- * the dialogue.
- * This slot is connected to the clicked() signal of the "OK" button.
- */
-void AutoCompletionDlg::accept()
-{
-	// Store widget values
-	m_nMinChars = m_pMinCharsSpin->value();
-	m_nDelay = m_pDelaySpin->value();
-	m_nMaxEntries = m_pMaxEntriesSpin->value();
-	
-	// Close the dialogue, indicating acceptance
-	QDialog::accept();
 }
 
 /**
@@ -275,6 +311,19 @@ int AutoCompletionDlg::exec()
 	// Show the dialogue
 	return QDialog::exec();
 }
-// END AutoCompletionDlg
 
-// Mon Jun 20 19:44:33 UTC 2011
+/**
+ * Stores the values set by the user in the dialogue widgets, and terminates
+ * the dialogue.
+ * This slot is connected to the clicked() signal of the "OK" button.
+ */
+void AutoCompletionDlg::accept()
+{
+	// Store widget values
+	m_nMinChars = m_pMinCharsSpin->value();
+	m_nDelay = m_pDelaySpin->value();
+	m_nMaxEntries = m_pMaxEntriesSpin->value();
+	
+	// Close the dialogue, indicating acceptance
+	QDialog::accept();
+}
