6525a4a25d37bee5ba97668f0f1cae49974272f4
Fred Ollinger <follinge@gmail.com>
2012-03-04 11:32:16 -0800

    Boilerplate filelistlocation added.

---
+++ src/filelistlocation.cpp
@@ -0,0 +1,15 @@
+#include "filelistlocation.h"
+
+namespace kscope4{
+FileListLocation::FileListLocation() : QWidget()
+{
+}
+
+/**
+ */
+FileListLocation::~FileListLocation() 
+{
+}
+
+} // namespace kscope4
+// Sun Mar  4 11:27:37 PST 2012
---
+++ src/filelistlocation.h
@@ -0,0 +1,19 @@
+#ifndef FILE_LIST_LOCATION_H
+#define FILE_LIST_LOCATION_H
+
+#include <QList>
+#include <QWidget>
+
+namespace kscope4{
+class FileListLocation : public QWidget
+{
+
+Q_OBJECT
+
+public:
+	FileListLocation();
+	~FileListLocation();
+};
+
+}
+#endif // FILE_LIST_LOCATION_H
