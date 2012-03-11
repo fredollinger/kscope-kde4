1d2439aecd0c7d24c1cb3bf3a38758c571725360
Fred Ollinger <follinge@gmail.com>
2012-03-11 13:03:07 -0700

    Renamed files.

---
+++ src/ksession.cpp
@@ -0,0 +1,14 @@
+#include "ksession.h"
+#include <QList>
+#include <QDebug>
+
+namespace kscope4{
+KSession::KSession() : QWidget()
+{
+}
+
+KSession::~KSession() 
+{
+}
+} // namespace kscope4
+// Sat Mar 10 19:22:41 PST 2012
---
+++ src/ksession.h
@@ -0,0 +1,29 @@
+#ifndef SESSION_H
+#define SESSION_H
+
+#include <QList>
+#include <QWidget>
+#include "filelistlocation.h"
+
+namespace kscope4{
+class KSession : public QWidget
+{
+
+Q_OBJECT
+
+public:
+	KSession();
+	~KSession();
+
+private:
+	FileListLocation fllOpenFiles;
+	QString sLastFile;
+	QStringList slQueryFiles;
+	QStringList slCallTreeFiles;
+	FileListLocation fllBookmarks;
+	QString sMakeCmd;
+	QString sMakeRoot;
+};
+
+}
+#endif // SESSION_H
